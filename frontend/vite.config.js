30bec8c728ab1e3701448298b9ab28add077c74c
Cristina Renda López <cristina.renda.lopez@gmail.com>
2025-03-10 12:18:25 +0100

    Fix: Add Vue structure and configure Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,15 @@
-export default {
-    build: {
-      rollupOptions: {
-        output: {
-          entryFileNames: 'assets/main.js',
-          chunkFileNames: 'assets/[name].js',
-          assetFileNames: 'assets/[name].[ext]'
-        }
-      }
-    }
-  };
-  
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue()],  // Agrega el plugin de Vue
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/main.js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
+});
