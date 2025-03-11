a3680539fbb625179e24d204191b0e55a5343d9c
Cristina Renda López <cristina.renda.lopez@gmail.com>
2025-03-11 08:59:05 +0100

    Cleanup and README update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [vue()],  // Agrega el plugin de Vue
+  plugins: [vue()],
   build: {
     rollupOptions: {
       output: {
